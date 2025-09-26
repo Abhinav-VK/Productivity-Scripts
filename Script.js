485db062896a38b4feab919efa7a803dc9c168f0
Abhinav-VK <abhinavpodicheti@gmail.com>
2025-09-26 14:01:54 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -15,8 +15,8 @@
 // @grant       GM_addStyle
 // @grant       GM_setValue
 // @grant       GM_getValue
-// @updateURL   https://raw.githubusercontent.com/Abhinav-VK/Productivity-Scripts/refs/heads/main/Combined%20Productivity%20Scripts.js
-// @downloadURL https://raw.githubusercontent.com/Abhinav-VK/Productivity-Scripts/refs/heads/main/Combined%20Productivity%20Scripts.js
+// @updateURL   https://raw.githubusercontent.com/Abhinav-VK/Productivity-Scripts/refs/heads/main/Script.js
+// @downloadURL https://raw.githubusercontent.com/Abhinav-VK/Productivity-Scripts/refs/heads/main/Script.js
 
 // ==/UserScript==
 
