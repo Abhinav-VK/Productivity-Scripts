f9abceb27f98e4f0fb5406c912b96082506dc6e5
Abhinav-VK <abhinavpodicheti@gmail.com>
2026-02-28 14:34:25 +0530

    8.1.3

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Combined Productivity Scripts
 // @namespace   http://tampermonkey.net/
-// @version     8.1.2
+// @version     8.1.3
 // @description Combines Hygiene Checks, RCAI Expand Findings, RCAI Results Popup, Serenity ID Extractor, SANTOS Checker, Check Mapping, Open RCAI and ILAC Auto Attach with Alt+X toggle panel
 // @author      Abhinav
 // @include     https://paragon-*.amazon.com/hz/view-case?caseId=*
